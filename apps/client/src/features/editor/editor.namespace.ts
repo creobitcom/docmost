ca073d13ec1aa22c906ff2cc30aef510299f639b
artyom-kalman <p.a.kalashnikov@ya.ru>
2025-05-16 15:21:33 +0900

    Update extentions list

--- apps/client/src/features/editor/editor.namespace.ts
+++ apps/client/src/features/editor/editor.namespace.ts
@@ -1,49 +1,85 @@
-import { Plugin, PluginKey } from 'prosemirror-state'
-import { Node as ProseMirrorNode } from 'prosemirror-model'
+import { Plugin, PluginKey } from "prosemirror-state";
+import { Node as ProseMirrorNode } from "prosemirror-model";
 
 export enum BlockType {
-  Paragraph = 'paragraph',
-  Heading = 'heading',
-  Blockquote = 'blockquote',
-  CodeBlock = 'codeBlock',
-  BulletList = 'bulletList',
-  OrderedList = 'orderedList',
-  ListItem = 'listItem',
-  TaskList = 'taskList',
-  TaskItem = 'taskItem',
-  HorizontalRule = 'horizontalRule',
-  Image = 'image',
-  Table = 'table',
-  TableRow = 'tableRow',
-  TableCell = 'tableCell',
-  TableHeader = 'tableHeader',
-  Iframe = 'iframe',
-  Figure = 'figure',
-  N8N = 'n8n',
+  Paragraph = "paragraph",
+  Heading = "heading",
+  Blockquote = "blockquote",
+  CodeBlock = "codeBlock",
+  BulletList = "bulletList",
+  OrderedList = "orderedList",
+  ListItem = "listItem",
+  TaskList = "taskList",
+  TaskItem = "taskItem",
+  HorizontalRule = "horizontalRule",
+  Image = "image",
+  Table = "table",
+  TableRow = "tableRow",
+  TableCell = "tableCell",
+  TableHeader = "tableHeader",
+  Iframe = "iframe",
+  Figure = "figure",
+  N8N = "n8n",
+  StarterKit = "starterKit",
+  Placeholder = "placeholder",
+  TextAlign = "textAlign",
+  Underline = "underline",
+  LinkExtension = "link",
+  Superscript = "superscript",
+  SubScript = "subscript",
+  Highlight = "highlight",
+  Typography = "typography",
+  TrailingNode = "trailingNode",
+  GlobalDragHandle = "globalDragHandle",
+  TextStyle = "textStyle",
+  Color = "color",
+  SlashCommand = "slashCommand",
+  EmojiCommand = "emojiCommand",
+  Comment = "comment",
+  Mention = "mention",
+  MathInline = "mathInline",
+  MathBlock = "mathBlock",
+  Details = "details",
+  DetailsSummary = "detailsSummary",
+  DetailsContent = "detailsContent",
+  Youtube = "youtube",
+  TiptapVideo = "video",
+  Callout = "callout",
+  CustomCodeBlock = "customCodeBlock",
+  Selection = "selection",
+  Attachment = "attachment",
+  Drawio = "drawio",
+  Excalidraw = "excalidraw",
+  Embed = "embed",
+  MarkdownClipboard = "markdownClipboard",
+  CharacterCount = "characterCount",
 }
+
 export const BlockTypes = Object.values(BlockType);
 
 export const UpdateBlockPositions = new Plugin({
-  key: new PluginKey('updateBlockPositionsPlugin'),
+  key: new PluginKey("updateBlockPositionsPlugin"),
 
   appendTransaction(transactions, oldState, newState) {
-    if (!transactions.some(tr => tr.docChanged)) return;
+    if (!transactions.some((tr) => tr.docChanged)) return;
 
     const tr = newState.tr;
     let modified = false;
 
-    newState.doc.content.forEach((node: ProseMirrorNode, offset: number, index: number) => {
-      const currentPos = node.attrs?.position;
-      if (currentPos !== index) {
-        const pos = tr.mapping.map(offset + 1)
-        tr.setNodeMarkup(pos - 1, undefined, {
-          ...node.attrs,
-          position: index,
-        })
-        modified = true;
-      }
-    })
-    
+    newState.doc.content.forEach(
+      (node: ProseMirrorNode, offset: number, index: number) => {
+        const currentPos = node.attrs?.position;
+        if (currentPos !== index) {
+          const pos = tr.mapping.map(offset + 1);
+          tr.setNodeMarkup(pos - 1, undefined, {
+            ...node.attrs,
+            position: index,
+          });
+          modified = true;
+        }
+      },
+    );
+
     return modified ? tr : null;
   },
-})
+});
